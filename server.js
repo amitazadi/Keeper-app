027d1a53b49cc88381cf8befa9e4287a10386e03
Amit <khurjaamit@gmail.com>
2021-04-08 09:08:59 +0530

    Final version of keeper app 4

--- server.js
+++ server.js
@@ -54,12 +54,12 @@ app.post("/delete", function (req, res) {
   });
 });
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 3001;
 
